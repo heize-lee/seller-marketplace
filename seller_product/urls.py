3a68000ed96d4bca938c1d01d7a34c776116fd76
hateyoon <yoyo00830@gmail.com>
2024-05-08 16:33:59 +0900

    base 수정

--- seller_product/urls.py
+++ seller_product/urls.py
@@ -1,16 +1,19 @@
-
-from django.urls import path 
+from django.urls import path
 from . import views
 
 app_name = 'product'
 
 urlpatterns = [
+    # 상품 목록 페이지
     path('', views.ProductList.as_view(), name='product'),
-    path('search/<str:q>/', views.ProductSearch.as_view()),
+    # 상품 상세 페이지
     path('<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-    path('create/', views.ProductCreate.as_view(), name='product_create'),
-    path('modify-cart/',views.modify_cart,name='modify_cart'),
-    # path('<int:pk>/',views.ProductDetail.as_view()),
-
-    path('category/<str:slug>/',views.category_page)
+    # 상품 등록 페이지
+    path('create/', views.ProductCreate.as_view(), name='product_create'),  # ProductCreate 뷰를 사용합니다.
+    # 카트 수정 뷰
+    path('modify-cart/', views.modify_cart, name='modify_cart'),
+    # 카테고리 페이지
+    path('category/<str:slug>/', views.category_page, name='category_page'),
 ]
+
+
