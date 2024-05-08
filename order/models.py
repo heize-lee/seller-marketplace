44d81dc4de831d66df5e06a71f315c2edb8451d4
GordPark <gord23p@gmail.com>
2024-05-08 14:58:28 +0900

    주문리스트, 주문 상세보기 업데이트

--- order/models.py
+++ order/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
-from seller_product.models import Product
+from seller_product.models import Product, Cart
 # Create your models here.
 
 class Order(models.Model):
     # settings에 있는 user_model 참조
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # product앱에 있는 Product 참조
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    # cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    total_price = models.IntegerField(default=0)
+    payment_total_price = models.IntegerField(default=0)    
+    amount = models.SmallIntegerField(default=0)
     order_date = models.DateField()
     payment_date = models.DateField()
     pay_confirm = models.BooleanField()
+
+    # def save(self, *args, **kwargs):
+    #     # Order를 저장할 때 Cart의 amount 값을 가져와서 Order의 amount 값으로 설정
+    #     self.amount = self.cart.amount if self.cart else 0
+    #     super().save(*args, **kwargs)
--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name='order'
 urlpatterns = [
     path('', views.order, name='order'),    
+    path('password_confirm/', views.password_confirm, name='password_confirm'),    
     path('order_done/', views.order_done, name='order_done'),    
-    
+    path('order_detail/<int:pk>/', views.order_detail, name='order_detail'),    
+    path('order_list/', views.order_list, name='order_list'),    
 ]
