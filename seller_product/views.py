7124373bfc1f84e313180a79ba96bcc143d0e92c
GordPark <gord23p@gmail.com>
2024-05-08 23:28:35 +0900

    navbar category, product/views.py, navbar_link 업데이트

--- seller_product/views.py
+++ seller_product/views.py
@@ -100,7 +100,9 @@ def modify_cart(request):
         product = Product.objects.get(pk=product_id)
         cart, _ = Cart.objects.get_or_create(user=user, product=product)    
         cart.amount=int(request.POST['count'])
+        cart.total_price = cart.amount * product.price
         if cart.amount>0:
+            cart.total_price = cart.amount * product.price
             cart.save()
         # order 뷰로 리디렉션
         return redirect('/order/')
