1f0567ebfa6074df7c14262f1f588d23cdf4a95e
hateyoon <yoyo00830@gmail.com>
2024-05-09 00:40:30 +0900

    config view.py추가

---
+++ config/views.py
@@ -0,0 +1,9 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic.edit import FormView, UpdateView
+from seller_product.models import Product,Category, Cart
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
