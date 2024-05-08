bd1a4d54eef048a66122901602d1fe9e77d98eb4
ansghltjd <ansghltjd9@naver.com>
2024-05-08 16:23:22 +0900

    코멘트

--- seller_product/admin.py
+++ seller_product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category,Cart
+from .models import Product, Category,Cart, Comment
 
  
 class ProductAdmin(admin.ModelAdmin):
@@ -14,6 +14,8 @@ class CategoryAdmin(admin.ModelAdmin):
  
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Cart)
+
+admin.site.register(Comment)
 
 
 
--- seller_product/models.py
+++ seller_product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+
 class Category(models.Model):
     category_name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -54,4 +55,12 @@ class Cart(models.Model):
     payment_total_price=models.IntegerField(default=0)
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Product, on_delete=models.CASCADE)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.autor}::{self.content}'
