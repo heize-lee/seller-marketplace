a37b412ce6c61839643fb0edb6078cfe9c5b7645
ansghltjd <ansghltjd9@naver.com>
2024-06-04 20:58:12 +0900

    Review모델, ReviewComment모델

--- web/reviews/models.py
+++ web/reviews/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
 # Create your models here.
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    # order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i,i) for i in range(1,6)])
+    comment = models.TextField()
+    image = models.ImageField(upload_to='review/images/%Y/%m/%d/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user}-{self.product}'
+
+class ReviewComment(models.Model):
+    review = models.ForeignKey(Review,on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    comment = models.TextField()
