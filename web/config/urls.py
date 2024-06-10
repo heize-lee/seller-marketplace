0be3f2cef523134ca8b34ec819ac5215cd68a3a5
jh <caivi@naver.com>
2024-06-10 10:56:00 +0900

    detaildone

--- web/config/urls.py
+++ web/config/urls.py
@@ -19,6 +19,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('product/', include('products.urls', namespace='product')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
