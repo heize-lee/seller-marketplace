982181ca58d26a12be6c62bc75ecf03b50ce9df2
GordPark <gord23p@gmail.com>
2024-06-21 16:22:22 +0900

    배송지 수정 모달-배송지 추가하기 업데이트

--- web/orders/views.py
+++ web/orders/views.py
@@ -15,6 +15,7 @@ def orders(request):
     if request.method == 'GET':
         user = request.user
         cart = Cart.objects.filter(user=user)
+        delivery_address = DeliveryAddress.objects.all()
 
         # if 바로구매 boolean True
         # 단일건에 해당되는 카트 튜플만 가져오고
@@ -22,9 +23,9 @@ def orders(request):
 
         try:
             # 기본 배송지 정보를 가져옵니다. (존재하지 않으면 except 블록으로 이동)
-            delivery_address = DeliveryAddress.objects.filter(user=user, is_default=True).last()
+            default_delivery_address = DeliveryAddress.objects.filter(user=user, is_default=True)[0]
         except DeliveryAddress.DoesNotExist:
-            delivery_address = None
+            default_delivery_address = None
 
         # js코드 복잡해짐
         # 등록된 배송지가 없음 > 지워지고 
@@ -35,7 +36,8 @@ def orders(request):
         context = {
             'cart': cart,
             'user': user,
-            'default_delivery_address': delivery_address,
+            'default_delivery_address': default_delivery_address,
+            'delivery_address': delivery_address,
         }
         return render(request,'orders/orders.html', context)
     
@@ -48,7 +50,12 @@ def orders(request):
         postal_code = request.POST.get('postal_code')
         address = request.POST.get('address')
         detailed_address = request.POST.get('detailed_address')
+        # 처음 배송지 추가시 disabled된 input-hidden으로 기본배송지 추가
         is_default = True if request.POST.get('default_delivery_hidden') == 'on' else False
+        # 수정모달에서 배송지 추가시 기본 배송지 체크여부에 따라 기본배송지 변경 
+        # 이전에 is_default가 True인 기본배송지는 False로 변경
+        # is_default = True if request.POST.get('default_delivery') == 'on' else False
+
 
         # 기본 배송지가 있는지 확인
         # if is_default:
