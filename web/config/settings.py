babcfa44f8bfbaf998cdaa801757bf31973f10f2
dev.heize <dev.heize@Heize-MacBook-Pro.local>
2024-06-24 04:03:18 +0900

    Update settings

--- web/config/settings.py
+++ web/config/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
