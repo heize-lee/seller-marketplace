87fe957284b498720c70501b190525a9fbbd3b69
dev.heize <dev.heize@Heize-MacBook-Pro.local>
2024-06-24 04:31:22 +0900

    Update settings

--- web/config/urls.py
+++ web/config/urls.py
@@ -5,6 +5,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import edit_profile  # edit_profile 뷰를 import  #재웅
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,7 +18,6 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+    re_path(r'^static/(?:.*)$', serve, {'document_root': settings.STATIC_ROOT, },
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
