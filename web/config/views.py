1f1a3c9146f3f9cbdafef327125b287eeb9bb7b2
jh <caivi@naver.com>
2024-06-24 14:55:44 +0900

    homepage

--- web/config/views.py
+++ web/config/views.py
@@ -1,8 +1,17 @@
+from random import sample
 from django.shortcuts import render
-from products.models import Category, Product
+from products.models import Category, Product  
 
 def home(request):
     categories = Category.objects.all()
-    products = Product.objects.all()  # 모든 상품을 가져옴
-    return render(request, 'home.html', {'categories': categories, 'products': products})
-
+    products = Product.objects.all()  
+    
+  
+    random_products = sample(list(products), min(len(products), 9))
+    
+    context = {
+        'categories': categories,
+        'random_products': random_products  
+    }
+    
+    return render(request, 'home.html', context)
