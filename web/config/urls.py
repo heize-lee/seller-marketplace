601540ae03c84552cb73fba4d684e9caa819ff34
dev.heize <dev.heize@Heize-MacBook-Pro.local>
2024-06-25 13:18:07 +0900

    media

--- web/config/urls.py
+++ web/config/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 from accounts.views import edit_profile  # edit_profile 뷰를 import  #재웅
 from django.views.static import serve
 from django.urls import re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,3 +24,6 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?:.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
